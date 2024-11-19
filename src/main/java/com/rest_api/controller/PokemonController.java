8397919c7121b5a8b98201c872630c080a01bed1
fabin0casa <fabiocasa999@gmail.com>
2024-11-19 18:21:00 -0300

    Criando os endpoints na classe controller assim como uma consulta customizada no repository e as regras de negocio no service, resolvendo a issue #9

--- src/main/java/com/rest_api/controller/PokemonController.java
+++ src/main/java/com/rest_api/controller/PokemonController.java
@@ -1,24 +1,63 @@
 package com.rest_api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.rest_api.entity.Pokemon;
 import com.rest_api.service.PokemonService;
 
 @RestController
+@RequestMapping("/pokemon")
 public class PokemonController {
 
     @Autowired
-    PokemonService pokemonService;
+    private PokemonService pokemonService;
 
-    @GetMapping("/pokemons")
-    public ResponseEntity<List<Pokemon>> getAllPokemons(){
+    //consultar todos
+    @GetMapping("/all")
+    public ResponseEntity<List<Pokemon>> getAllPokemons() {
         List<Pokemon> pokemons = pokemonService.getAllPokemonService();
         return ResponseEntity.ok(pokemons);
+    }
+
+    //consultar pelo numero da PokeDex
+    @GetMapping("/{numeroDex}")
+    public ResponseEntity<Optional<Pokemon>> getPokemon(@PathVariable int numeroDex) {
+        Optional<Pokemon> pokemon = pokemonService.getPokemonByNumeroDex(numeroDex);
+        if (pokemon.isPresent()) {
+            return ResponseEntity.ok(pokemon);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    //cadastrar
+    @PostMapping("/add")
+    public ResponseEntity<Pokemon> addPokemon(@RequestBody Pokemon pokemon) {
+        Pokemon newPokemon = pokemonService.addPokemonService(pokemon);
+        return new ResponseEntity<>(newPokemon, HttpStatus.CREATED);
+    }
+
+    //atualizar
+    @PutMapping("/update")
+    public ResponseEntity<Pokemon> updatePokemon(@RequestBody Pokemon pokemon) {
+        Pokemon updatedPokemon = pokemonService.updatePokemonService(pokemon);
+        if (updatedPokemon != null) {
+            return ResponseEntity.ok(updatedPokemon);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    //deletar
+    @DeleteMapping("/delete/{numeroDex}")
+    public ResponseEntity<Void> deletePokemon(@PathVariable int numeroDex) {
+        pokemonService.deletePokemonByNumeroDex(numeroDex);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/rest_api/service/PokemonService.java
+++ src/main/java/com/rest_api/service/PokemonService.java
@@ -1,6 +1,7 @@
 package com.rest_api.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,4 +20,39 @@ public class PokemonService {
         return pokemons;
     }
 
+    public Optional<Pokemon> getPokemonByNumeroDex(int numeroDex) {
+        return pokemonRepository.findByNumeroDex(numeroDex);
+    }
+
+    public Pokemon addPokemonService(Pokemon pokemon) {
+        //Definindo a url da imagem com base no numeroDex
+        String urlImagem = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" 
+                            + pokemon.getNumeroDex() + ".png";
+        pokemon.setUrlImagem(urlImagem);
+    
+        return pokemonRepository.save(pokemon);
+    }
+    
+
+    public void deletePokemonByNumeroDex(int numeroDex) {
+        Optional<Pokemon> pokemon = pokemonRepository.findByNumeroDex(numeroDex);
+        pokemon.ifPresent(p -> pokemonRepository.delete(p));
+    }
+
+    public Pokemon updatePokemonService(Pokemon pokemon) {
+        Optional<Pokemon> existingPokemon = pokemonRepository.findByNumeroDex(pokemon.getNumeroDex());
+        if (existingPokemon.isPresent()) {
+            Pokemon updatedPokemon = existingPokemon.get();
+            updatedPokemon.setNome(pokemon.getNome());
+            updatedPokemon.setEspecie(pokemon.getEspecie());
+            updatedPokemon.setTipoPrimario(pokemon.getTipoPrimario());
+            updatedPokemon.setTipoSecundario(pokemon.getTipoSecundario());
+            updatedPokemon.setDescricao(pokemon.getDescricao());
+            updatedPokemon.setAltura(pokemon.getAltura());
+            updatedPokemon.setPeso(pokemon.getPeso());
+            updatedPokemon.setUrlImagem(pokemon.getUrlImagem());
+            return pokemonRepository.save(updatedPokemon);
+        }
+        return null;
+    }
 }
