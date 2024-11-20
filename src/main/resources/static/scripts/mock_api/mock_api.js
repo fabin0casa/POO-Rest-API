0899f1a6fd2ea6f1358ad1f6bf167faf0d0ba6ff
EthanHueh <nattanssouzapvz@gmail.com>
2024-11-20 20:43:37 -0300

    Ajustes no script de mock

--- src/main/resources/static/scripts/mock_api/mock_api.js
+++ src/main/resources/static/scripts/mock_api/mock_api.js
@@ -2,25 +2,25 @@ function loadData(){
     return [
         {
             "nome": "Pikachu",
-            "numero_dex": 25,
+            "numeroDex": 25,
             "especie": "Mouse Pokémon",
-            "tipo_primario": "Elétrico",
-            "tipo_secundario": null,
+            "tipoPrimario": "Elétrico",
+            "tipoSecundario": null,
             "descricao": "Pikachu é um Pokémon de tipo Elétrico. É um dos Pokémon mais conhecidos e é a mascote da franquia.",
             "altura": 0.40,
             "peso": 6.00,
-            "url_imagem": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+            "urlImagem": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
         },
         {
             "nome": "Pidgey",
-            "numero_dex": 16,
+            "numeroDex": 16,
             "especie": "Tiny Bird",
-            "tipo_primario": "Normal",
-            "tipo_secundario": "Voador",
+            "tipoPrimario": "Normal",
+            "tipoSecundario": "Voador",
             "descricao": "Pidgey tem um senso de direção extremamente nítido.",
             "altura": 0.30,
             "peso": 1.80,
-            "url_imagem": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png"
+            "urlImagem": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png"
         }
     ];
 }
@@ -28,13 +28,13 @@ function loadData(){
 function loadOneData(){
     return {
         "nome": "Pikachu",
-        "numero_dex": 25,
+        "numeroDex": 25,
         "especie": "Mouse Pokémon",
-        "tipo_primario": "Elétrico",
-        "tipo_secundario": null,
+        "tipoPrimario": "Elétrico",
+        "tipoSecundario": null,
         "descricao": "Pikachu é um Pokémon de tipo Elétrico. É um dos Pokémon mais conhecidos e é a mascote da franquia.",
         "altura": 0.40,
         "peso": 6.00,
-        "url_imagem": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+        "urlImagem": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
     };
 }
