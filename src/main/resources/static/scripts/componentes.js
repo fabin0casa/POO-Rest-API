fa6ec997bfa5c698e1170b1ffcd039bd9093046b
EthanHueh <nattanssouzapvz@gmail.com>
2024-11-20 19:54:23 -0300

    Refatoracao do codigo do front

---
+++ src/main/resources/static/scripts/componentes.js
@@ -0,0 +1,142 @@
+function criarPokedexEntry(pokemon){
+    
+    //Criando a pokedexEntry
+    const pokedexEntry = document.createElement('div');
+    pokedexEntry.className = 'pokedex-entry';
+
+    const pokedexImagem = document.createElement('div');
+    pokedexImagem.className = 'pokedex-imagem';
+
+    //Criando a secao da imagem
+    const img = document.createElement('img');
+    img.src = pokemon.urlImagem;
+    pokedexImagem.append(img);
+
+    //Criando o conteudo da pokedex
+    const pokedexConteudo = document.createElement('div');
+    pokedexConteudo.className = 'pokedex-conteudo';
+
+    let p = document.createElement('p');
+    
+    p.textContent = pokemon.numeroDex+" "+pokemon.nome;
+    pokedexConteudo.append(p);
+
+    p.textContent = pokemon.especie;
+    pokedexConteudo.append(p);
+    
+    p = document.createElement('p');
+    let span = document.createElement('span');
+    span.textContent = pokemon.tipoPrimario;
+    p.append(span);
+
+    if (pokemon.tipoSecundario !== null){
+        span = document.createElement('span');
+        span.textContent = pokemon.tipoSecundario;
+        p.append(span);
+    }
+
+    pokedexConteudo.append(p);
+
+    p = document.createElement('p');
+    span = document.createElement('span');
+    span.textContent = 'Altura';
+    p.append(span);
+
+    span = document.createElement('span');
+    span.textContent = pokemon.altura;
+    p.append(span);
+    pokedexConteudo.append(p);
+
+    p = document.createElement('p');
+    span = document.createElement('span');
+    span.textContent = "Peso";
+    p.append(span);
+
+    span = document.createElement('span');
+    span.textContent = pokemon.peso;
+    p.append(span);
+    pokedexConteudo.append(p);
+
+    //Criando secao da descricao
+    const pokedexDescricao = document.createElement('div');
+    pokedexDescricao.className = 'pokedex-descricao';
+    pokedexDescricao.textContent = pokemon.descricao;
+
+    //Retornando
+    pokedexEntry.append(pokedexImagem);
+    pokedexEntry.append(pokedexConteudo);
+    pokedexEntry.append(pokedexDescricao);
+
+    return pokedexEntry;
+}
+
+function criarFormularioAtributos(){
+    const form = document.createElement('form');
+
+    ATRIBUTOS_POKEMON.forEach(atributo => {
+        const div = document.createElement('div');
+        const label = document.createElement('label');
+
+        switch (atributo.nome) {
+            default:
+                label.textContent = atributo.nomeExibicao+":";
+                label.setAttribute('for', atributo.nome);
+                
+                const input = document.createElement('input');
+                input.id = atributo.nome;
+                input.name = atributo.nome;
+
+                switch (atributo.nome) {
+                    case 'numeroDex':
+                    case 'altura':
+                    case 'peso':
+                        input.type = 'number'
+                        break;
+
+                    case 'urlImagem':
+                        input.type = 'url';
+                        break;
+                
+                    default:
+                        input.type = 'text';
+                        break;
+                }
+
+                div.append(label);
+                div.append(input);
+
+                form.append(div);
+                break;
+        
+            case 'tipoPrimario':
+            case 'tipoSecundario':
+                label.textContent = atributo.nomeExibicao+":";
+                label.setAttribute('for', atributo.nome);
+                div.append(label);
+
+                const dropDownTipos = criarDropdownTipos();
+
+                dropDownTipos.id = atributo.nome;
+                dropDownTipos.name = atributo.nome;
+                div.append(dropDownTipos);
+                
+                form.append(div);
+                break;
+        }
+    });
+
+    return form;
+}
+
+function criarDropdownTipos(){
+
+    const dropDownTipos = document.createElement("select");
+
+    TIPOS_POKEMON.forEach(tipo => {
+        const opcao = document.createElement('option');
+        opcao.text = tipo;
+        dropDownTipos.append(opcao);
+    });
+
+    return dropDownTipos;
+}
---
+++ src/main/resources/static/scripts/pokemonInfo.js
@@ -0,0 +1,59 @@
+const ATRIBUTOS_POKEMON = [
+    {
+        "nome":"nome",
+        "nomeExibicao":"Nome",
+    },
+    {
+        "nome":"numeroDex",
+        "nomeExibicao":"Número Dex",
+    },
+    {
+        "nome":"especie",
+        "nomeExibicao":"Espécie",
+    },
+    {
+        "nome":"tipoPrimario",
+        "nomeExibicao":"Tipo Primário",
+    },
+    {
+        "nome":"tipoSecundario",
+        "nomeExibicao":"Tipo Secundário",
+    },
+    {
+        "nome":"descricao",
+        "nomeExibicao":"Descrição",
+    },
+    {
+        "nome":"altura",
+        "nomeExibicao":"Altura",
+    },
+    {
+        "nome":"peso",
+        "nomeExibicao":"Peso",
+    },
+    {
+        "nome":"urlImagem",
+        "nomeExibicao":"URL da Imagem",
+    }
+];
+
+const TIPOS_POKEMON = [
+    "Normal",
+    "Fogo",
+    "Água",
+    "Elétrico",
+    "Grama",
+    "Gelo",
+    "Lutador",
+    "Veneno",
+    "Terra",
+    "Voador",
+    "Psíquico",
+    "Inseto",
+    "Pedra",
+    "Fantasma",
+    "Dragão",
+    "Sombrio",
+    "Metal",
+    "Fada"
+];
